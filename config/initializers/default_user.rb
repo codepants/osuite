9166ab3770764954b9be7e02a5ce8c7a34571f0d
boss <han@odore.ai>
2025-02-07 02:28:45 +0800

    temp rem def user

--- config/initializers/default_user.rb
+++ config/initializers/default_user.rb
@@ -1,13 +1,13 @@
 Rails.application.config.after_initialize do
-  if defined?(User) && !Rails.env.test?
-    unless User.exists?(email: 'me@me.com')
-      User.create!(
-        email: 'me@me.com',
-        password: 'password',
-        password_digest: BCrypt::Password.create("password"),
-        name: 'me',
-        admin: true
-      )
-    end
-  end
+  # if defined?(User) && !Rails.env.test?
+  #   unless User.exists?(email: 'me@me.com')
+  #     User.create!(
+  #       email: 'me@me.com',
+  #       password: 'password',
+  #       password_digest: BCrypt::Password.create("password"),
+  #       name: 'me',
+  #       admin: true
+  #     )
+  #   end
+  # end
 end 
